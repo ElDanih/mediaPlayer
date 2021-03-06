dd9808425da5a92f2617cf8657c122fe37ad1bd7
ElDanih <danandrescalderon@gmail.com>
2021-03-06 15:28:33 -0500

    Guardado por precaucion

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,12 +1,26 @@
 function MediaPlayer(config) {
-    this.media = config.element;
-  }
-  
-  MediaPlayer.prototype.playAndPause = function () {
-      (this.media.paused)
-          ? this.media.play() 
-          : this.media.pause();
-  };
+  this.media = config.element;
+  this.plugins = config.plugins || [];
 
+  this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function () {
+  this.plugins.forEach( plugin => {
+    plugin.run();
+  });
+};
+
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false;
+};
+
+MediaPlayer.prototype.playAndPause = function () {
+  this.media.paused ? this.media.play() : this.media.pause();
+};
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,7 @@
-import MediaPlayer from './MediaPlayer.js';
+import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/AutoPlay.js'
 
 const video = document.querySelector("video")
 const button = document.querySelector("button")
-const player = new MediaPlayer({ element: video })
+const player = new MediaPlayer({ element: video, plugins : [ new AutoPlay] })
 button.onclick = () => player.playAndPause()
